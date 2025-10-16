d3353afee11023a8aa4edae55c70cb60ac1956f8
matlav <mathis.laversin92@gmail.com>
2025-10-16 16:45:57 +0200

    improve schema and fix packages

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { prisma } from './prisma';
 
 export const auth = betterAuth({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://localhost:3000',
     database: prismaAdapter(prisma, {
         provider: 'postgresql',
     }),
