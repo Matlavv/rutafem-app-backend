10804019fe564409586a03c24038189ee2c4e8df
matlav <mathis.laversin92@gmail.com>
2025-11-05 10:57:23 +0100

    link ride with users and add API testing with Bruno

--- src/routes/ride.routes.ts
+++ src/routes/ride.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { rideController } from '../controllers/ride.controller';
+import { requireAuth } from '../middleware/auth.middleware';
 
 const router = Router();
 
-router.post('/', (req, res, next) => rideController.create(req, res, next));
+// Protected routes (require authentication)
+router.post('/', requireAuth, (req, res, next) => rideController.create(req, res, next));
+router.patch('/:id', requireAuth, (req, res, next) => rideController.update(req, res, next));
+router.delete('/:id', requireAuth, (req, res, next) => rideController.delete(req, res, next));
+
+// Public routes
 router.get('/', (req, res, next) => rideController.findAll(req, res, next));
 router.get('/:id', (req, res, next) => rideController.findById(req, res, next));
-router.patch('/:id', (req, res, next) => rideController.update(req, res, next));
-router.delete('/:id', (req, res, next) => rideController.delete(req, res, next));
 
 export default router;
-
